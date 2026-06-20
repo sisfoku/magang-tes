7cd77919e6c46e9afe05aeed908d96b519a62e76
yudarahmasisfo <yudarahma@sisfo.untan.ac.id>
2026-06-20 17:57:42 +0700

    ds

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Aplikasi Is Running! Code Next';
+    return 'Aplikasi Is Running! Code Next is working!';
   }
 }
