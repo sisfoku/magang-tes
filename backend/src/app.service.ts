583a788a307d2ff903061c96a3d860db5afde391
yudarahmasisfo <yudarahma@sisfo.untan.ac.id>
2026-06-22 13:39:32 +0700

    aplikasi testing

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'APlikasi berjalan dengan baik';
+    return 'Aplikasi ku berjalan';
   }
 }
